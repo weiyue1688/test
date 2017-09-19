2aa5b890d921fe8c92c597df23e3e50748bb4751
weiyue <weiyue_1688@163.com>
2017-09-19 20:42:19 +0800

    初始导入

---
+++ ee-xpd-manager-web/src/main/java/com/itwy/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.itwy.controller;
+
+import org.junit.Test;
+
+public class TestController {
+	public static void main(String[] args) {
+		System.out.println("I Love You!");
+		System.out.println("You Love Me!");
+		System.out.println("We Love World!");
+	}
+	@Test
+	public void test001(){
+		System.out.println("One World");
+	}
+
+	public void test003(){
+		System.out.println("We are family");
+	}
+
+	public void test002(){
+		System.out.println("One Dream");
+	}
+
+}
